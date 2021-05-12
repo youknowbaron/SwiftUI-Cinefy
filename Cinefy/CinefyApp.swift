d8f3b0dbae29dc1c4856a64bd6163cca11d6d1fe
bach.vo <bach.vo@executionlab.asia>
2021-05-12 14:33:25 +0700

    Initial Commit

---
+++ Cinefy/CinefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinefyApp.swift
+//  Cinefy
+//
+//  Created by vobach on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CinefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
